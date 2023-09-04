6a6e412c57f56e72234f62040c6e2511af09c953
bootcamp_Candra Eka dwi warsa <candraeka932@gmail.com>
2023-09-04 11:52:08 +0700

    pertama

---
+++ projectpertama/Program.cs
@@ -0,0 +1,13 @@
+﻿using Cat;
+
+class Program
+{
+	static void Main()
+	{
+		cat cibi = new cat();
+		cibi.meow();
+		cat burhan = new cat();
+		burhan.Jump();
+		
+	}
+}
---
+++ projectpertama/cat.cs
@@ -0,0 +1,20 @@
+namespace Cat;
+
+public class cat
+{
+
+	public string color;
+	public int age;
+	
+	public bool isStrip;
+	
+	public void meow()
+	{
+		Console.WriteLine("Meowww");
+	}
+	public void Jump()
+	{
+		Console.WriteLine("Jump");
+	}
+
+}
