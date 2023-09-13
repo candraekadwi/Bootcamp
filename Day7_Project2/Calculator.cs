fdebc765201e954f0b8df64671c033faf85b1f8d
bootcamp_Candra Eka dwi warsa <candraeka932@gmail.com>
2023-09-13 11:33:29 +0700

    Day7- Return Type

---
+++ Day7_Project2/Calculator.cs
@@ -0,0 +1,13 @@
+namespace Day7_Project2;
+
+public class Calculator
+{
+	public static int Sum(int a, int b)
+	{
+		return a+b;
+	}
+	public static int Multiplay(int a, int b)
+	{
+		return a*b;
+	}
+}
---
+++ Day7_Project2/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using Day7_Project2;
+
+public delegate int MyDelegate(int a, int b);
+
+class Program
+{
+	static void Main()
+	{
+		MyDelegate proses = Calculator.Sum;
+		
+		proses +=Calculator.Multiplay;
+		
+		Delegate[] data = proses.GetInvocationList();
+		
+		List<int> result=new();
+		
+		foreach(MyDelegate x in data)
+		{
+			result.Add(x.Invoke(4,7));
+		}
+		foreach(int i in result)
+		{
+			Console.WriteLine(i);
+		}
+	}
+}
