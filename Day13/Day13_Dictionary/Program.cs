5f63caf757c82045b9f570b11f605cedc215487f
bootcamp_Candra Eka dwi warsa <candraeka932@gmail.com>
2023-09-20 11:45:33 +0700

    Day13-Colletion|| Dictionary, Queue, Stack

---
+++ Day13/Day13_Dictionary/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+	static void Main()
+	{
+		//Dictionary
+		
+		Dictionary<int,string> dict = new Dictionary<int, string>();
+		
+		dict.Add(2, "wow");
+		dict.Add(7,"ass");
+		dict.Add(3,"fofo");
+		foreach(var x in dict)
+		{
+			Console.WriteLine($"key {x.Key}, value {x.Value}");
+		}
+		Console.WriteLine("");
+		dict.TryAdd(2,"faku");
+		dict[3]="bebe";
+		dict.Remove(7);
+		foreach(var x in dict)
+		{
+			Console.WriteLine($"key {x.Key}, value {x.Value}");
+		}
+		bool haha=dict.TryGetValue(3,out string y);
+		Console.WriteLine(haha);
+		Console.WriteLine(y);
+	}
+}
---
+++ Day13/Day13_Queue_Stack/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+class Program
+{
+	static void Main()
+	{
+		// Qoueue => urutan dari pertama/atas
+		Queue<int> x = new Queue<int>();
+		
+		x.Enqueue(3);
+		x.Enqueue(4);
+		x.Enqueue(7);
+		x.Enqueue(5);
+		x.Enqueue(8);
+		
+		Console.WriteLine(x.Dequeue()); //3 mengambil value
+		Console.WriteLine(x.Dequeue()); //4 mengambil value
+		Console.WriteLine(x.Peek()); //7 melihat value
+		Console.WriteLine(x.Peek());//7 meliat value
+		Console.WriteLine(x.Dequeue());//7 mengambil value
+		Console.WriteLine(x.Dequeue());//5 mengambil value
+		
+		Console.WriteLine("");
+		
+		//Stack => urutan dari terakhir/ bawah
+		Stack<int> y = new Stack<int>();
+		y.Push(4);
+		y.Push(8);
+		y.Push(3);
+		y.Push(2);
+		
+		Console.WriteLine(y.Pop());//2
+		Console.WriteLine(y.Pop());//3
+		Console.WriteLine(y.Peek());//8
+		Console.WriteLine(y.Pop());//8
+		Console.WriteLine(y.Pop());//4
+		
+	}
+}
