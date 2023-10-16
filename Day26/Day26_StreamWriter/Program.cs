fb34a4320f3627039ab09e625e738058da1142ae
candraekadwi <candraeka932@gmail.com>
2023-10-16 11:30:19 +0700

    Day26-Stream

---
+++ Day26/Day26_StreamWriter/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+	static void Main()
+	{
+		using (FileStream fs = new(@"./haha.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite))
+		{
+			using (StreamWriter sw = new StreamWriter(fs))
+			{
+				var mystring = File.ReadAllLines(@"./haha.txt").ToList();
+				string x = "awalan";
+				var newstring = x + mystring;
+				sw.WriteLine(newstring);
+
+			}
+		}
+	}
+}
---
+++ Day26/Dsay26_FileSytream/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Dynamic;
+using System.IO;//filestream
+using System.Text;//filestream
+
+class Program
+{
+	static void Main()
+	{
+		string x ="";
+		while(true){
+		Console.Write("input Text: ");
+		x=Console.ReadLine();
+		using(FileStream fs= new(@"./abcd.txt",FileMode.Append,FileAccess.Write))
+		{
+			byte[] bytes=Encoding.UTF8.GetBytes(x);
+			fs.Write(bytes,0,bytes.Length);
+			Console.WriteLine($"succes");
+		}
+		using(FileStream fs2= new(@"./abcd.txt",FileMode.Open,FileAccess.Read))
+		{
+			byte[] bytes2=new byte[fs2.Length];
+			fs2.Read(bytes2,0,bytes2.Length);
+			string y= Encoding.UTF8.GetString(bytes2);
+			Console.WriteLine($"dat:{y}");
+		}
+		using(FileStream fs2= new(@"./abcd.txt",FileMode.Open,FileAccess.Read,FileShare.Read))
+		{
+			while(true)
+			{
+				
+			}
+		}
+		
+	}
+	}
+}
